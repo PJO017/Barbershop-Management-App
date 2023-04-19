fa0752d2b2aedbd995c4732dbbbd314fe939066b
Erys Dennis <edennis6@student.gsu.edu>
2023-04-19 14:47:42 -0400

    recyclerview from FB collection

---
+++ app/src/main/java/com/example/barbershopmanagementapp/FavoriteActivity.java
@@ -0,0 +1,29 @@
+package com.example.barbershopmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+
+        /*
+         * pull list of faves from FB
+         *
+         * creates a new box (View TBD) for each barber
+         *   same format for barbers
+         * */
+    }
+    public void removeFave(View view){
+        /*
+        * change color of start to white
+        *
+        * remove from FB
+        * */
+    }
+}
