0c4821d7d231a9777924e41ea637ca01a87cb7c0
Erys Dennis <edennis6@student.gsu.edu>
2023-04-21 13:11:39 -0400

    story 8 functionality

--- app/src/main/java/com/example/barbershopmanagementapp/BarberActivity.java
+++ app/src/main/java/com/example/barbershopmanagementapp/BarberActivity.java
@@ -2,54 +2,57 @@ package com.example.barbershopmanagementapp;
 
 import static android.content.ContentValues.TAG;
 
-import static com.example.barbershopmanagementapp.R.id.barberRV;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseRecyclerOptions;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BarberActivity extends AppCompatActivity {
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String email;
+    FirebaseAuth mAuth;
+    Button bt;
 
-    private FirebaseFirestore db;
-
-    RecyclerView barberRV;
-    List<Barber> barberList;
-    MyAdapter barberAdapter;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barber);
 
-        db = FirebaseFirestore.getInstance();
-        barberList = new ArrayList<>();
-
-        barberRV = findViewById(R.id.barberRV);
-        barberRV.setLayoutManager(new LinearLayoutManager(this));
-
-        barberAdapter = new MyAdapter(this, barberList);
-        barberRV.setAdapter(barberAdapter);
+        layout = findViewById(R.id.layout);
 
         db.collection("Barbers").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -62,8 +65,8 @@ public class BarberActivity extends AppCompatActivity {
                             Log.d(TAG, "onSuccess: " + snapshot.getString("Name"));
                             Log.d(TAG, "onSuccess: " + snapshot.getLong("Rating"));
 
-                            Barber barber = snapshot.toObject(Barber.class);
-                            barberList.add(barber);
+                            createBarbers(snapshot.getString("Name"), String.valueOf(snapshot.getLong("Rating")));
+
 
                         }
                     }
@@ -74,81 +77,92 @@ public class BarberActivity extends AppCompatActivity {
                     }
                 });
 
+        TextView show = findViewById(R.id.favorites);
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FavoriteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
+    private void createBarbers(String name, String rating) {
+        Log.d(TAG, "VIEW CREATED" + name);
+        View view = getLayoutInflater().inflate(R.layout.barber_view, null);
 
+//        email  = mAuth.getCurrentUser().getEmail();
+        email = "user@email.com"; //to be deleted for testing purposes
 
-//        List<Barber> barbers = new ArrayList<Barber>();
-//        List<String> names = new ArrayList<String>();
-//        List<String> rates = new ArrayList<String>();
-//        barbers.add(new Barber("John", "3.5"));
-//        barbers.add(new Barber("jack", "3.7"));
-//        //gets list of all docs in barbers collection
-//        FirebaseFirestore.getInstance()
-//                .collection("Barbers")
-//                .get()
-//                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-//                        Log.d(TAG, "onSuccess: We're getting the data");
-//
-//                        List<DocumentSnapshot> snapshotList = queryDocumentSnapshots.getDocuments();
-//                        for (DocumentSnapshot snapshot: snapshotList) {
-//                            Log.d(TAG, "onSuccess: " + snapshot.getString("Name"));
-//                            Log.d(TAG, "onSuccess: " + snapshot.getLong("Rating "));
-//
-//
-//                            names.add(snapshot.getString("Name"));
-//                            rates.add(snapshot.getLong("Rating ").toString());
-//                            barbers.add(new Barber(snapshot.getString("Name"), snapshot.getLong("Rating ").toString()));
-//                            Log.d(TAG, "onSuccess: " + barbers.toString());
-//                            Log.d(TAG, "onSuccess: " + names.toString());
-//                            Log.d(TAG, "onSuccess: " + rates.toString());
-//
-//
-//                        }
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.e(TAG, "onFailure: ", e);
-//                    }
-//                });
-//
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView.setAdapter(new MyAdapter(getApplicationContext(), barbers));
+        TextView nameView = view.findViewById(R.id.nameView);
+        TextView ratingView = view.findViewById(R.id.ratingView);
+
+        Button likeBt = view.findViewById(R.id.faveBt);
+        Button reviewBt = view.findViewById(R.id.reviewBt);
+
+        nameView.setText(name);
+        ratingView.setText("Rating: " + rating);
+
+        likeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                like(name, email);
+            }
+        });
+
+        reviewBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchReview();
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.setBackgroundColor(getResources().getColor(R.color.newBack));
+                launchBooking();
+            }
+        });
+
+        layout.addView(view);
+
 
     }
 
+    private void like(String barbName, String email) {
+        Map<String, String> fave = new HashMap<>();
+        fave.put("Barber", barbName);
+        fave.put("User", email);
 
-    // customer clicks on star button to favorite barber
-    public void saveFave(View view) {
-        /*
-        * if this barber is already favorited
-        *   change star back to white
-        * else
-        *   change star to yellow
-        *
-        * add barber to customers favorites in FB
-        * */
+        db.collection("Favorites").add(fave)
+            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w(TAG, "Error adding document", e);
+                }
+            });
+
     }
 
-    //customer clicks on this to leave a review and rating for barber (not my US)
-    public void leaveReview(View view){
-        /*
-        * directs them to the reviews page
-        * */
+    //launches review
+    public void launchReview(){
+        Log.d(TAG, "review clicked");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
-    //customer clicks on "Choose" button at the bottom of the page in order to select them as a barber (not my US)
-    public void book(View view){
-        /*
-        * if this barber is already selected
-        *   un-highlight
-        * if this barber is selected
-        *   highlight                               simply marking that this barber was chosen for booking
-        *
-        * direct them to booking page
-        * */
+
+    //launches booking
+    public void launchBooking(){
+        Log.d(TAG, "booking clicked");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
