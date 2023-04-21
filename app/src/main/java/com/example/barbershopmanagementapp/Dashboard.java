e26aa8607b2e5731039592173b67f24f6f7d768e
AbdulMalek <M_2008_HA@YAHOO.COM>
2023-04-21 18:44:18 -0400

    Integrated firestore database, Edit and Delete functionalities can modify the database now

--- app/src/main/java/com/example/barbershopmanagementapp/Dashboard.java
+++ app/src/main/java/com/example/barbershopmanagementapp/Dashboard.java
@@ -1,82 +1,198 @@
 package com.example.barbershopmanagementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.io.InputStream;
+import com.google.firebase.Timestamp;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.Map;
 
 public class Dashboard extends AppCompatActivity {
+
+    private LinearLayout itemList;
+    private List<Customer> customerList;
+    private Map<Customer, View> customerViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        LinearLayout itemList = findViewById(R.id.item_list);
+        itemList = findViewById(R.id.item_list);
+        customerViews = new HashMap<>();
 
-        List<Customer> customerList = loadCustomersFromXml();
+        customerList = new ArrayList<>();
+        loadCustomersFromFirebase();
 
+        Button saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveChanges(customerList);
+            }
+        });
+    }
+
+    private void loadCustomersFromFirebase() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference customersRef = db.collection("Appointments");
+
+        customersRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if (task.isSuccessful()) {
+                    Dashboard.this.customerList.clear();
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Customer customer = document.toObject(Customer.class);
+                        customer.setDocumentId(document.getId());
+                        Log.d("CustomerData", "Fetched customer: " + customer.toString());
+                        Dashboard.this.customerList.add(customer);
+                    }
+
+                    // Sorting the items of the list here so that the earliest appointment render at the top, or so I hope.
+                    Collections.sort(customerList);
+
+                    // This is to update the ui and view the changes before saving them.
+                    updateUI(customerList);
+                } else {
+                    Log.e("Dashboard", "Error loading appointments: ", task.getException());
+                    Toast.makeText(Dashboard.this, "An error occurred. Please try again later.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void updateUI(List<Customer> customerList) {
         for (Customer customer : customerList) {
             View itemView = LayoutInflater.from(this).inflate(R.layout.activity_appointment_item, null);
-
+            customerViews.put(customer, itemView); // Store the view for each customer
             TextView customerName = itemView.findViewById(R.id.customer_name);
             TextView appointmentTime = itemView.findViewById(R.id.appointment_time);
-            ImageButton deleteButton = itemView.findViewById(R.id.delete_button);
+            ImageButton optionsButton = itemView.findViewById(R.id.delete_button);
 
-            customerName.setText(customer.getName());
-            appointmentTime.setText(customer.getAppointmentTime());
-            deleteButton.setOnClickListener(new View.OnClickListener() {
+            customerName.setText(customer.getCustomer());
+            appointmentTime.setText(customer.getFormattedDate());
+
+            optionsButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // TODO: Delete the customer's appointment
+                    // Create and show the popup menu
+                    PopupMenu popupMenu = new PopupMenu(Dashboard.this, optionsButton);
+                    popupMenu.inflate(R.menu.customer_options_menu);
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.edit_option:
+                                    EditCustomerFragment editCustomerFragment = new EditCustomerFragment(customer, new EditCustomerFragment.OnEditCompleteListener() {
+                                        @Override
+                                        public void onEditComplete(Customer customer, Timestamp newDate) {
+                                            customer.setDate(newDate);
+                                            customer.setModified(true);
+                                            updateCustomerUI(customer); // Update the UI for this specific customer
+                                        }
+                                    });
+                                    editCustomerFragment.show(getSupportFragmentManager(), "EditCustomerFragment"); // This line was missing
+                                    return true;
+
+                                case R.id.delete_option:
+                                    customer.setMarkedForDeletion(true);
+                                    itemView.setVisibility(View.GONE);
+                                    return true;
+                                default:
+                                    return false;
+                            }
+                        }
+                    });
+
+                    popupMenu.show();
                 }
             });
+
             itemList.addView(itemView);
         }
     }
 
-
-    private List<Customer> loadCustomersFromXml() {
-        List<Customer> customerList = new ArrayList<>();
-        try {
-            // Get the XML file as a stream
-            InputStream inputStream = getResources().openRawResource(R.raw.customers);
-
-            // Create a new XML document builder
-            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-
-            // Parse the XML file into a document
-            Document document = builder.parse(inputStream);
-
-            // Get a list of all customer elements in the document
-            NodeList customerNodes = document.getElementsByTagName("customer");
-
-            // Loop through each customer element and create a Customer object for it
-            for (int i = 0; i < customerNodes.getLength(); i++) {
-                Element customerElement = (Element) customerNodes.item(i);
-                String name = customerElement.getElementsByTagName("name").item(0).getTextContent();
-                String appointmentTime = customerElement.getElementsByTagName("appointment_time").item(0).getTextContent();
-                customerList.add(new Customer(name, appointmentTime));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void updateCustomerUI(Customer customer) {
+        View itemView = customerViews.get(customer);
+        if (itemView != null) {
+            TextView appointmentTime = itemView.findViewById(R.id.appointment_time);
+            appointmentTime.setText(customer.getFormattedDate());
         }
-        return customerList;
     }
 
+    private void saveChanges(List<Customer> customerList) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference customersRef = db.collection("Appointments");
+
+        List<Task<Void>> tasks = new ArrayList<>();
+
+        for (Customer customer : this.customerList) {
+            if (customer.isMarkedForDeletion()) {
+                String documentId = customer.getDocumentId();
+                Task<Void> deleteTask = customersRef.document(documentId).delete();
+                tasks.add(deleteTask);
+            } else if (customer.isModified()) {
+                String documentId = customer.getDocumentId();
+                // Only update the specific fields that have changed
+                Map<String, Object> updates = new HashMap<>();
+                updates.put("Date", customer.getDate());
+                Task<Void> updateTask = customersRef.document(documentId).update(updates);
+                tasks.add(updateTask);
+            }
+        }
+        Toast.makeText(Dashboard.this, "Changes Saved!", Toast.LENGTH_SHORT).show();
+
+
+        // Wait for all tasks (deletions and updates) to complete before refreshing the UI
+        Tasks.whenAll(tasks).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    // Remove the deleted customers from the local list
+                    Iterator<Customer> iterator = customerList.iterator();
+                    while (iterator.hasNext()) {
+                        Customer customer = iterator.next();
+                        if (customer.isMarkedForDeletion()) {
+                            iterator.remove();
+                        } else if (customer.isModified()) {
+                            customer.setModified(false);
+                        }
+                    }
+
+                    itemList.removeAllViews();
+                    loadCustomersFromFirebase();
+                } else {
+                    // Handle any errors
+                    Log.e("Dashboard", "Error updating appointments: ", task.getException());
+                }
+            }
+        });
+    }
 }
